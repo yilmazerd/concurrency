2355e11d97305cb3921db0daefa9c876f397c1c4
yilmazerd <yilmazerdem@gmail.com>
2020-12-20 23:57:58 -0500

    No change

--- src/main/java/com/example/concurrency/ConcurrencyApplication.java
+++ src/main/java/com/example/concurrency/ConcurrencyApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConcurrencyApplication {
 
+    //
     public static void main(String[] args) {
         SpringApplication.run(ConcurrencyApplication.class, args);
     }
