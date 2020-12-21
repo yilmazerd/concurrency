c790c88457ca30c4f4d8b09204b79216ac4b4bda
yilmazerd <yilmazerdem@gmail.com>
2020-12-21 00:23:40 -0500

    Add simple thread

--- src/main/java/com/example/concurrency/BasicThread.java
+++ src/main/java/com/example/concurrency/BasicThread.java
@@ -1,4 +1,29 @@
 package com.example.concurrency;
 
 public class BasicThread {
+    public static void main(String[] args) {
+
+        SomeClass someClass1 = new SomeClass();
+        SomeClass someClass2 = new SomeClass();
+        SomeClass someClass3 = new SomeClass();
+        SomeClass someClass4 = new SomeClass();
+        SomeClass someClass5 = new SomeClass();
+        someClass1.start();
+        someClass2.start();
+        someClass3.start();
+        someClass4.start();
+        someClass5.start();
+
+    }
+
+
+
+}
+
+
+class SomeClass extends Thread {
+    @Override
+    public void run(){
+        System.out.println("Executing run method in some class, thread is: " + Thread.currentThread());
+    }
 }
--- src/main/java/com/example/concurrency/BasicWithRunnable.java
+++ src/main/java/com/example/concurrency/BasicWithRunnable.java
@@ -1,4 +1,42 @@
 package com.example.concurrency;
 
 public class BasicWithRunnable {
+    public static void main(String[] args) {
+        SomeRunnable someRunnable = new SomeRunnable();
+        Thread thread1 = new Thread(someRunnable);
+        Thread thread2 = new Thread(someRunnable);
+        Thread thread3 = new Thread(someRunnable);
+        Thread thread4 = new Thread(someRunnable);
+        Thread thread5 = new Thread(someRunnable);
+        Thread thread6 = new Thread(someRunnable);
+        Thread thread7 = new Thread(someRunnable);
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
+        thread5.start();
+        thread6.start();
+        thread7.start();
+        System.out.println("Thread state :" + thread1.getState().toString());
+        System.out.println("Thread name :" + thread1.getName().toString());
+        System.out.println("Thread id :" + thread1.getId());
+        System.out.println("Thread priority :" + thread1.getPriority());
+        System.out.println("Thread group :" + thread1.getThreadGroup());
+        try {
+            Thread.sleep(100);
+        } catch (Exception e) {
+            //
+        }
+        System.out.println("Thread state :" + thread1.getState().toString());
+    }
+
+}
+
+
+class SomeRunnable implements  Runnable {
+    @Override
+    public void run() {
+        System.out.println("Executing run method in some class, thread is: " + Thread.currentThread());
+        System.out.println("State of the thread is: " + Thread.currentThread().getState().toString());
+    }
 }
